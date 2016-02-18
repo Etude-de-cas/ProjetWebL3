96071555af0f34d5daba4456d0097b523e2a78d4
Remy Jerome <remy.jerome@live.com>
2016-02-18 13:19:20 +0100

    Ajout projet symfony et bootstrap

---
+++ www/bayangam/src/wp/AssociationBundle/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace wp\AssociationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('wpAssociationBundle:Home:index.html.twig');
+    }
+}
