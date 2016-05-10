ea871d40819851a64081fdb46ca25ff98df38337
Remy Jerome <remy.jerome@live.com>
2016-05-10 11:55:08 +0200

    Test

--- www/bayangam/src/wp/AssociationBundle/Controller/HomeController.php
+++ www/bayangam/src/wp/AssociationBundle/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace wp\AssociationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class HomeController extends Controller
 {
